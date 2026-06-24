36d7b0e51385017b7c3983d954131328dfe27989
Kanishka Chauhan <kanishkachauhan006@gmail.com>
2026-06-24 16:05:44 +0530

    Create PartitionList.cpp

---
+++ LinkedList/PartitionList.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode before_head(0),after_head(0);
+        ListNode*before=&before_head;
+        ListNode*after=&after_head;
+
+        while(head){
+            if(head->val<x){
+                before->next=head;
+                before=before->next;
+            }
+            else{
+                after->next=head;
+                after=after->next;
+            }
+            head=head->next;
+
+        }
+        after->next=nullptr;
+        before->next=after_head.next;
+        return before_head.next;
+    }
+};
