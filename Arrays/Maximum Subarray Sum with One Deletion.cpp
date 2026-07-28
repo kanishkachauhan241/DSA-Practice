235270c94c6a7723f20448d3e71ec04baa5e37dd
Kanishka Chauhan <kanishkachauhan006@gmail.com>
2026-07-28 22:09:26 +0530

    Create Maximum Subarray Sum with One Deletion.cpp

---
+++ Arrays/Maximum Subarray Sum with One Deletion.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& arr) {
+        int noDelete=arr[0];
+        int oneDelete=0;
+        int ans=arr[0];
+        for(int i=1;i<arr.size();i++){
+            oneDelete=max(noDelete,oneDelete+arr[i]);
+            noDelete=max(arr[i],noDelete+arr[i]);
+            ans=max(ans,max(noDelete,oneDelete));
+        }
+        return ans;
+    }
+};
