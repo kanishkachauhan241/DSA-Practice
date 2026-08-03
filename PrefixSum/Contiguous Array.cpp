20a20e72ca79a4d78fcfa9932004fcfb5294d500
Kanishka Chauhan <kanishkachauhan006@gmail.com>
2026-08-03 23:21:47 +0530

    Create Contiguous Array.cpp

---
+++ PrefixSum/Contiguous Array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+
+        unordered_map<int, int> mp;
+        mp[0]=-1;
+        int prefixSum=0;
+        int maxLength=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                prefixSum+=-1;
+            else
+                prefixSum+=1;
+            if(mp.find(prefixSum)!=mp.end()){
+                maxLength=max(maxLength,i-mp[prefixSum]);
+            } else{
+                mp[prefixSum]=i;
+            }
+        }
+        return maxLength;
+    }
+};
