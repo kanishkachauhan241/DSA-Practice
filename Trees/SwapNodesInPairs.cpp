ef5056fb7aff9a6cf5a17d0e17ddd198c6d601fa
Kanishka Chauhan <kanishkachauhan006@gmail.com>
2026-08-17 23:31:58 +0530

    Implement swapPairs function to swap nodes in pairs

---
+++ Trees/SwapNodesInPairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head){
+        ListNode* dummy=new ListNode(0);
+        dummy->next=head;
+        ListNode* prev=dummy;
+        while(prev->next!=nullptr &&
+               prev->next->next!=nullptr){
+            ListNode* first=prev->next;
+            ListNode* second=first->next;
+            first->next=second->next;
+            second->next=first;
+            prev->next=second;
+            prev=first;
+        }
+        return dummy->next;
+    }
+};
